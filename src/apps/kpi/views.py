35052b12d8e0957806aa8db45ec9ee8c26b9b50e
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2024-11-01 15:55:03 -0600

    Originación de la tabla de datos.
    1. Se agrega la consulta al contexto que se envía a la plantilla.
    2. Se prueban los datos del QuerySet en la plantilla.
    
    Caso #103.

--- src/apps/kpi/views.py
+++ src/apps/kpi/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
+from .models import KPI
 
 
 class KpiIndex(TemplateView):
     template_name = 'kpi/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'KPI'
+        context['kpi'] = KPI.objects.filter(active=True)
+        return context
