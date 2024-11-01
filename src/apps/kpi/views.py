946b22959f55ca36d5e053156db8b1d6226bcf5e
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2024-11-01 11:23:24 -0600

    Se crea la vista inicial de kpi
    Caso #106

--- src/apps/kpi/views.py
+++ src/apps/kpi/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class KpiIndex(TemplateView):
+    template_name = 'kpi/index.html'
