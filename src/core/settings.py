af61fb2f036ad0610d23a1e9cca3ae489b6da7a7
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2024-11-01 10:14:27 -0600

    El namespace correcto es apps.kpi.apps.KpiConfig.
    Se corrige.
    
    Caso #102

--- src/core/settings.py
+++ src/core/settings.py
@@ -56,7 +56,7 @@ LOCAL_APPS = [
     'apps.ideas.config.IdeasConfig',
     'apps.carto.config.CartoConfig',
     'apps.pas.config.PasConfig',
-    'apps.kpi.config.KpiConfig',
+    'apps.kpi.apps.KpiConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
