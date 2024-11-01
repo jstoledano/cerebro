53588faa5c7ea2a92c460de2d465a7616b131cf6
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2024-11-01 10:11:49 -0600

    Se agrega apps.kpi a LOCAL_APPS en settings.py
    Caso #102

--- src/core/settings.py
+++ src/core/settings.py
@@ -56,6 +56,7 @@ LOCAL_APPS = [
     'apps.ideas.config.IdeasConfig',
     'apps.carto.config.CartoConfig',
     'apps.pas.config.PasConfig',
+    'apps.kpi.config.KpiConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
